e2605075990387d444c61add53907bafc331d2f3
bruderj15 <julian.bruder@outlook.com>
2026-01-29 22:44:54 +0100

    feat#4: implement simple ner/ned

---
+++ apps/qanary-component-nerd-simple/src/handler.ts
@@ -0,0 +1,150 @@
+import type {IQanaryComponentMessageHandler} from "@leipzigtreechat/qanary-component-core";
+import {createAnnotationInKnowledgeGraph, type IAnnotationInformation} from "@leipzigtreechat/qanary-component-helpers";
+import {getQuestion, type IQanaryMessage, QANARY_PREFIX} from "@leipzigtreechat/shared";
+
+/**
+ * An event handler for incoming messages of the Qanary pipeline
+ * Exported only for testing purposes
+ * @param message incoming qanary pipeline message
+ */
+// eslint-disable-next-line sonarjs/no-invariant-returns
+export const handler: IQanaryComponentMessageHandler = async (message: IQanaryMessage) => {
+  console.log(message);
+
+  const question = await getQuestion(message);
+  if (!question) {
+    console.warn("No question found in message.");
+    return message;
+  }
+  console.log("Question:", question);
+
+  const nerdAnnotations: Array<IAnnotationInformation> = await getNerdAnnotations(question);
+  console.log(`NED/NER for question '${question}':`, nerdAnnotations);
+
+  for (const annotation of nerdAnnotations) {
+    await createAnnotationInKnowledgeGraph({
+      message: message,
+      componentName: "qanary-component-nerd-simple",
+      annotation,
+      annotationType: `${QANARY_PREFIX}AnnotationOfNerd`,
+    })
+  }
+
+  return message;
+};
+
+const getNerdAnnotations = async (question: string): Promise<Array<IAnnotationInformation>> => {
+  switch (question) {
+    case "Wie viel wurde im Stadtteil Connewitz gegossen?":
+      return [
+        {
+          value: JSON.stringify({
+            entity: "Connewitz",
+            type: "Stadtteil",
+          }),
+          range: {start: 28, end: 37},
+          confidence: 1
+        },
+      ]
+    case "Welche Wasserentnahmestellen gibt es in der Nähe der Adresse Karl-Liebknecht-Str. 132, 04277 Leipzig?":
+      return [
+        {
+          value: JSON.stringify({
+            entity: "Wasserentnahmestelle",
+            type: "Infrastruktur",
+          }),
+          range: {start: 7, end: 28},
+          confidence: 1
+        },
+        {
+          value: JSON.stringify({
+            entity: "Karl-Liebknecht-Str.",
+            type: "Straße",
+          }),
+          range: {start: 61, end: 81},
+          confidence: 1
+        },
+        {
+          value: JSON.stringify({
+            entity: "132",
+            type: "Hausnummer",
+          }),
+          range: {start: 82, end: 85},
+          confidence: 1
+        },
+        {
+          value: JSON.stringify({
+            entity: "04277",
+            type: "Postleitzahl",
+          }),
+          range: {start: 87, end: 92},
+          confidence: 1
+        },
+        {
+          value: JSON.stringify({
+            entity: "Leipzig",
+            type: "Stadt",
+          }),
+          range: {start: 93, end: 100},
+          confidence: 1
+        },
+      ]
+    case "Welchen Baum kann ich in der Nähe der Adresse Karl-Liebknecht-Str. 132, 04277 Leipzig heute gießen?":
+      return [
+        {
+          value: JSON.stringify({
+            entity: "Baum",
+            type: "Infrastruktur",
+          }),
+          range: {start: 8, end: 12},
+          confidence: 1
+        },
+        {
+          value: JSON.stringify({
+            entity: "Karl-Liebknecht-Str.",
+            type: "Straße",
+          }),
+          range: {start: 46, end: 66},
+          confidence: 1
+        },
+        {
+          value: JSON.stringify({
+            entity: "132",
+            type: "Hausnummer",
+          }),
+          range: {start: 87, end: 90},
+          confidence: 1
+        },
+        {
+          value: JSON.stringify({
+            entity: "04277",
+            type: "Postleitzahl",
+          }),
+          range: {start: 72, end: 77},
+          confidence: 1
+        },
+        {
+          value: JSON.stringify({
+            entity: "Leipzig",
+            type: "Stadt",
+          }),
+          range: {start: 78, end: 85},
+          confidence: 1
+        },
+      ]
+    case "Was kannst du mir über die Bäume in Leipzig erklären?":
+      return [
+        {
+          value: JSON.stringify({
+            entity: "Baum",
+            type: "Vegetation",
+          }),
+          range: {start: 27, end: 32},
+          confidence: 1
+        },
+      ]
+    default:
+      console.warn("Unrecognized question:", question);
+      return []
+  }
+};
