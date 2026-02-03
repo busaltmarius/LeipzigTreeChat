e22752ece76bf6458b5f890753d8d5eb0ad91e27
Eric Behrendt <eric@behrendt.me>
2026-02-03 18:04:46 +0100

    feat(qanary):started implementation of qanary-component-sparql-generation

---
+++ apps/qanary-component-sparql-generation/src/handler.ts
@@ -0,0 +1,61 @@
+import type { IQanaryComponentMessageHandler } from "@leipzigtreechat/qanary-component-core";
+import { createAnnotationInKnowledgeGraph, selectSparql, getEndpoint } from "@leipzigtreechat/qanary-component-helpers";
+import { getQuestion, type IQanaryMessage, QANARY_EAT_PREFIX, QANARY_PREFIX } from "@leipzigtreechat/shared";
+import { URL } from "url";
+
+/**
+ * An event handler for incoming messages of the Qanary pipeline
+ * Exported only for testing purposes
+ * @param message incoming qanary pipeline message
+ */
+// eslint-disable-next-line sonarjs/no-invariant-returns
+export const handler: IQanaryComponentMessageHandler = async (message: IQanaryMessage) => {
+  console.log(message);
+
+  const get_relation_query = ""
+
+  // Step 1: get relation annotation from qanary triplestore
+  const relationResponse = await selectSparql({
+    endpointUrl: getEndpoint(message),
+    query: get_relation_query
+  });
+
+  const relation = relationResponse
+
+  const template = await mapRelationToTemplate(relation)
+
+  const get_answer_query = template
+
+  const componentName = "qanary-component-sparql-generation"
+  await createAnnotationInKnowledgeGraph({
+    message: message,
+    componentName: componentName,
+    annotation: {
+      value: get_answer_query,
+    },
+    annotationType: QANARY_PREFIX + "AnnotationOfSparqlQuery"
+  })
+
+  const answerResponse = await selectSparql({
+    endpointUrl: "",
+    query: get_answer_query
+  })
+
+  const answer = answerResponse
+
+  await createAnnotationInKnowledgeGraph({
+    message: message,
+    componentName: componentName,
+    annotation: {
+      value: answer,
+    },
+    annotationType: QANARY_PREFIX + "AnnotationOfAnswer"
+  })
+
+
+  return message;
+};
+
+export const mapRelationToTemplate = async(relation: string) => {
+
+}
