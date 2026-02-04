43bd103b39fce26b2465d2358a91d6e32c99801a
Eric Behrendt <eric@behrendt.me>
2026-02-04 16:59:58 +0100

    feat:#8 #9 first simple implementation of sparql generation and execution

--- apps/qanary-component-sparql-generation/src/handler.ts
+++ apps/qanary-component-sparql-generation/src/handler.ts
@@ -1,7 +1,6 @@
 import type { IQanaryComponentMessageHandler } from "@leipzigtreechat/qanary-component-core";
-import { createAnnotationInKnowledgeGraph, getEndpoint, selectSparql } from "@leipzigtreechat/qanary-component-helpers";
+import { createAnnotationInKnowledgeGraph, selectSparql, getEndpoint, getOutGraph } from "@leipzigtreechat/qanary-component-helpers";
 import { type IQanaryMessage, QANARY_PREFIX } from "@leipzigtreechat/shared";
-
 /**
  * An event handler for incoming messages of the Qanary pipeline
  * Exported only for testing purposes
@@ -11,36 +10,48 @@ import { type IQanaryMessage, QANARY_PREFIX } from "@leipzigtreechat/shared";
 export const handler: IQanaryComponentMessageHandler = async (message: IQanaryMessage) => {
   console.log(message);
 
-  const get_relation_query = "";
+  const graphId = getOutGraph(message)
+
+  const getDataQuery = `
+  PREFIX qa: <http://www.wdaqua.eu/qa#>
+  PREFIX oa: <http://www.w3.org/ns/openannotation/core/>
+  SELECT ?relationType ?entityId WHERE {
+    GRAPH <${graphId}> {
+      ?relationAnnotationId a qa:AnnotationOfRelation ;
+        oa:hasBody ?relationType .
+      ?entityAnnotationId a qa:AnnotationOfEntity ;
+        oa:hasBody ?entityId .
+    }
+  }
+  `
 
   // Step 1: get relation annotation from qanary triplestore
   const relationResponse = await selectSparql({
     endpointUrl: getEndpoint(message),
-    query: get_relation_query,
+    query: getDataQuery
   });
 
-  const relation = relationResponse;
+  const relation = relationResponse[0]?.relationType.value ?? null
+  const entityId = relationResponse[0]?.entityId.value ?? null
 
-  const template = await mapRelationToTemplate(relation);
+  const get_answer_query = await mapDataToTemplate(relation, entityId)
 
-  const get_answer_query = template;
-
-  const componentName = "qanary-component-sparql-generation";
+  const componentName = "qanary-component-sparql-generation"
   await createAnnotationInKnowledgeGraph({
     message: message,
     componentName: componentName,
     annotation: {
       value: get_answer_query,
     },
-    annotationType: `${QANARY_PREFIX}AnnotationOfSparqlQuery`,
-  });
+    annotationType: QANARY_PREFIX + "AnnotationOfSparqlQuery"
+  })
 
   const answerResponse = await selectSparql({
-    endpointUrl: "",
-    query: get_answer_query,
-  });
+    endpointUrl: "localhost:8000",
+    query: get_answer_query
+  })
 
-  const answer = answerResponse;
+  const answer = getAnswerAnnotations(relation, answerResponse)
 
   await createAnnotationInKnowledgeGraph({
     message: message,
@@ -48,10 +59,69 @@ export const handler: IQanaryComponentMessageHandler = async (message: IQanaryMe
     annotation: {
       value: answer,
     },
-    annotationType: `${QANARY_PREFIX}AnnotationOfAnswer`,
-  });
+    annotationType: QANARY_PREFIX + "AnnotationOfAnswer"
+  })
+
 
   return message;
 };
 
-export const mapRelationToTemplate = async (_relation: string) => {};
+export const mapDataToTemplate = async(relation: string, entityId: string): Promise<string> => {
+  if(relation == "Wie viel wurde im Stadteil Kleinzschocher gegossen?") {
+    return `
+    SELECT ?amount
+      WHERE {
+        ?treeId <urn:de:leipzig:trees:vocab:leipziggiesst:bezirk> "Kleinzschocher" ;
+          <urn:de:leipzig:trees:vocab:leipziggiesst:wassersumme> ?amount .
+      }
+    `
+  } else if(relation == "Welche Wasserentnahme Stellen gibt es in der Nähe der Adresse Karl-Liebknecht-Str. 132, 04277 Leipzig?") {
+    return ""
+  } else if(relation == "Welchen Baum kann ich in der Nähe der Adresse Karl-Liebknecht-Str. 132, 04277 Leipzig heute gießen?") {
+    return `
+    SELECT ?number ?long ?lat
+      WHERE {
+        ?s <urn:de:leipzig:trees:vocab:baumkataster:strasse> "Karl-Liebknecht-Straße" ;
+          <urn:de:leipzig:trees:vocab:baumkataster:baumnummer> ?number ;
+          <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long ;
+          <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat .
+      } 
+    `
+  } else if(relation == "Was kannst du mir über die Bäume in Leipzig erklären?") {
+    return `
+    SELECT DISTINCT ?species
+      WHERE {
+        ?treeId <urn:de:leipzig:trees:vocab:baumkataster:gattung> ?species .
+      }
+    `
+  }
+
+  return ""
+}
+
+export const getAnswerAnnotations = async (relation: string, response: any[]): Promise<string> => {
+  let answer = "";
+  
+  if (relation == "Wie viel wurde im Stadteil Kleinzschocher gegossen?") {
+    answer = response.reduce((sum, item) => sum + Number(item.amount?.value || 0), 0).toString();
+    
+  } else if (relation == "Welche Wasserentnahme Stellen gibt es in der Nähe der Adresse Karl-Liebknecht-Str. 132, 04277 Leipzig?") {
+    answer = "null";
+    
+  } else if (relation == "Welchen Baum kann ich in der Nähe der Adresse Karl-Liebknecht-Str. 132, 04277 Leipzig heute gießen?") {
+    const list = response.map((item) => ({
+      lat: parseFloat(item.lat?.value || "0"),
+      long: parseFloat(item.long?.value || "0"),
+      number: parseInt(item.number?.value || "0", 10)
+    }));
+    answer = JSON.stringify(list);
+    
+  } else if (relation == "Was kannst du mir über die Bäume in Leipzig erklären?") {
+    answer = response.map((item) => item.species?.value).join(", ");
+  }
+
+  return JSON.stringify({
+    value: answer,
+    confidence: 1
+  });
+};
