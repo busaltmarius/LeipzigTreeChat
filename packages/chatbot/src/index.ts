891cb30358cffc2c957f24a622ea4299bf4c7afd
Mose Schmiedel <mose.schmiedel@stud.htwk-leipzig.de>
2026-02-04 15:46:01 +0100

    fix(chatbot): add edge from start to Response node

--- packages/chatbot/src/index.ts
+++ packages/chatbot/src/index.ts
@@ -9,4 +9,5 @@ const { ResponseNode } = Nodes(START, END, CHATBOT_RESPONSE_NODE_ID);
 // Build the graph
 export const ChatBotGraph = new StateGraph(AgentStateAnnotation)
   .addNode(CHATBOT_RESPONSE_NODE_ID, ResponseNode({ nextNode: END }), { ends: [END] })
+  .addEdge(START, CHATBOT_RESPONSE_NODE_ID)
   .compile();
